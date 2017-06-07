201cca29f8bd238a06842a538da94c248fcd97e9
Rodrigo Dávila <rdavila92@gmail.com>
2017-06-07 21:21:16 +0200

    Añado el proyecto

---
+++ Assets/Scripts/Generate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Generate : MonoBehaviour
+{
+	public GameObject rocks;
+	int score = -3;
+	
+	// Use this for initialization
+	void Start()
+	{
+		InvokeRepeating("CreateObstacle", 1f, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void OnGUI () 
+	{
+		GUI.color = Color.black;
+		GUI.Label(new Rect (700, 150, 150,25), "Score: " + score.ToString());
+	}
+	
+	void CreateObstacle()
+	{
+		Instantiate(rocks);
+		score++;
+	}
+}
